29725a47c099b4008eef2d4f91738b91df976c96
Joel Meizer <joel.meizer@pia.ai>
2025-09-09 21:16:11 +1000

    commit to joels branch

---
+++ suggestion-service/src/main/java/CSIT318Project/suggestionService/SuggestionSpringApplication.java
@@ -0,0 +1,29 @@
+package CSIT318Project.suggestionservice;
+
+import CSIT318Project.bookService.model.Book;
+import CSIT318Project.bookService.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SuggestionSpringApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(SuggestionSpringApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner loadDatabase(BookRepository bookRepository) throws Exception {
+		return args -> {};
+	}
+
+}
