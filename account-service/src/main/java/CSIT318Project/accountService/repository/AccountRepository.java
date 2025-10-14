638a67a5328d189ae87e60301f62eb24751135f9
shakeelmahdhy <shakeelmahdhy@gmail.com>
2025-10-14 21:33:41 +1100

    Delete account-service directory

--- account-service/src/main/java/CSIT318Project/accountService/repository/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package CSIT318Project.accountService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Book, String> {
-}
